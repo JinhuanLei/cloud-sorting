06cb32e8228cef742ba8a7869869b626653ee769
Jinhuan Lei <jinhuanlei@gmail.com>
2020-08-28 15:21:31 -0500

    finish code change for data generator

--- DataGenerator/RandomNumberGenerator.java
+++ DataGenerator/RandomNumberGenerator.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 public class RandomNumberGenerator {
 
@@ -13,13 +14,20 @@ public class RandomNumberGenerator {
         }
     }
 
+    protected int generateNum() {
+        Random rand = new Random();
+        return rand.nextInt(Integer.MAX_VALUE);
+    }
+
     private void writeDataToFile() throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("tempfile.txt", true), 8192 * 4);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("dataset1.txt", true), 8192 * 4);
         StringBuffer sb = new StringBuffer();
-        for (int x = 0; x < 1000; x++){
-            sb.append("");
-            writer.append(sb.toString());
+        for (int x = 0; x < 50000000; x++){
+            sb.append(generateNum());
+            sb.append(" ");
         }
+        writer.append(sb.toString());
+
         writer.close();
     }
 }
